26614343b1a93d29fc7b046960bdedc02297a206
Antsje van der Leij <antsjevdleij@gmail.com>
2025-07-17 11:03:11 +0200

    Changed name SnailObject to SnailMeasurer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.reference_object import ReferenceObject
-from src.snail_object import SnailObject
+from src.snail_measurer import SnailMeasurer
 from src.utils import get_args, get_input_img
 import cv2
 import matplotlib.pyplot as plt
@@ -17,21 +17,27 @@ def main():
     pixels_per_metric = ref_obj.calculate_pixels_per_metric(image.copy())
     print(f"Pixels per metric: {pixels_per_metric}")
 
-    # Create a SnailObject instance and clip the petri dish
-    snail_obj = SnailObject()
+    # Create a SnailMeasurer instance and clip the petri dish
+    snail_obj = SnailMeasurer()
     snail_obj.pixels_per_metric = pixels_per_metric
 
-    petri_mask = snail_obj.clip_petri_dish(image.copy())
+    # petri_mask = snail_obj.clip_petri_dish(image.copy())
 
-    #TODO should be in a separate method in SnailObject
-    masked_image = cv2.bitwise_and(
-        image, image,
-        mask=petri_mask if len(petri_mask.shape) == 2 else cv2.cvtColor(petri_mask, cv2.COLOR_BGR2GRAY)
-    )
+    # #TODO should be in a separate method in SnailMeasurer
+    # masked_image = cv2.bitwise_and(
+    #     image, image,
+    #     mask=petri_mask if len(petri_mask.shape) == 2 else cv2.cvtColor(petri_mask, cv2.COLOR_BGR2GRAY)
+    # )
     # Prepare the image for contour detection
-    edged = snail_obj.prep_image(masked_image)
+    edged = snail_obj.prep_image(image)
+    
     annotated_image = image.copy()
-    snail_obj.get_snail_contours(edged, annotated_image)
+    snail_obj.get_snail_contours(edged, annotated_image, args["pos_key"])
+
+    # mask = snail_obj.prep_image(image)
+    # separated_mask = snail_obj.separate_touching_snails(mask)
+    # snail_obj.get_snail_contours(separated_mask,image.copy())
+
     # snail_obj.annotate_dimensions(annotated_image)
 
     # plt.imshow(cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB))
